182bbfa895e5e64190af9f5d57d84182ee282927
Benjamin Kircher <kircher@otris.de>
2014-10-07 11:38:11 +0200

    Compile with both, GCC and Clang

--- make.sh
+++ make.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-clang++-3.5 -O0 -g -std=c++14 -stdlib=libc++ \
+# Clang/libc++:
+
+clang++-3.5 -O0 -g -std=c++11 -stdlib=libc++ \
     -Wall -Wextra -Wshadow \
     test.cpp -o test \
     -lcurl
+
+# GCC/libstdc++:
+
+# g++ -O0 -g -std=c++11 \
+#     -Wall -Wextra -pedantic \
+#     test.cpp -o test \
+#     -lcurl
