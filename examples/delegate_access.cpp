e11fb29b5681044a61a3e6aa0ba467bb367fffd5
Marcel Schlitzer <schlitzer@otris.de>
2017-03-20 18:49:18 +0100

    Add delegate access example

---
+++ examples/delegate_access.cpp
@@ -0,0 +1,60 @@
+
+//   Copyright 2016 otris software AG
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#include <ews/ews.hpp>
+#include <ews/ews_test_support.hpp>
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+int main()
+{
+    int res = EXIT_SUCCESS;
+    ews::set_up();
+
+    try
+    {
+        const auto env = ews::test::environment();
+        auto service = ews::service(env.server_uri, env.domain, env.username,
+                                    env.password);
+        auto delegate = ews::delegate_user("test4@otris.de",
+                                           ews::mailbox("test1@otris.de"));
+        std::cout << "Delegator: " << delegate.get_delegator_address()
+                  << std::endl;
+        std::cout << "Delegated Account: " << delegate.get_delegated_account()
+                  << std::endl;
+        delegate.set_permission(
+            ews::standard_folder::calendar,
+            ews::delegator_permission_level::editor);
+		delegate.set_view_private_items(false);
+		delegate.set_receive_copies_of_meeting_messages(false);
+		auto resp = service.add_delegate(delegate);
+		std::cout << resp << std::endl;
+
+    }
+    catch (std::exception& exc)
+    {
+        std::cout << exc.what() << std::endl;
+        res = EXIT_FAILURE;
+    }
+
+    ews::tear_down();
+    return res;
+}
+
+// vim:et ts=4 sw=4
