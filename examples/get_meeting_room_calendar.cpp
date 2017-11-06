c12131aded4ea106c08b2b7e8bfec8374cde7977
Benjamin Kircher <kircher@otris.de>
2017-11-06 10:59:31 +0100

    get_meeting_room_calendar: typos; pimp example a bit

--- examples/get_meeting_room_calendar.cpp
+++ examples/get_meeting_room_calendar.cpp
@@ -33,24 +33,33 @@ int main()
         auto service = ews::service(env.server_uri, env.domain, env.username,
                                     env.password);
 
-        // We need to be able to acces the rooms mailbox
-        auto owner = ews::mailbox("meetingroom1@example.com");
+        // We need to be able to access the room's mailbox
+        auto owner = ews::mailbox("meetingroom1@otris.de");
 
         // Using the calendar_view we can limit the returned appointments
         // to a specific range of time
         // If we want all appointments, we leave out the calendar_view
         ews::distinguished_folder_id calendar_folder(
             ews::standard_folder::calendar, owner);
-        std::string start_date("2017-03-01T00:00:00-07:00");
-        std::string end_date("2017-03-31T23:59:59-07:00");
+        std::string start_date("2017-11-05T00:00:00-07:00");
+        std::string end_date("2017-11-10T23:00:00-07:00");
 
         // By using ews::base_shape::all_properties we get the full calendar
         // item, not only the item_id
         const auto found_items =
             service.find_item(ews::calendar_view(start_date, end_date),
                               calendar_folder, ews::base_shape::all_properties);
-        std::cout << "# calender items found: " << found_items.size()
+        std::cout << "# calendar items found: " << found_items.size()
                   << std::endl;
+
+        for (const auto& item : found_items)
+        {
+            std::cout << item.get_subject();
+            std::cout << ", start: " << item.get_start().to_string();
+            std::cout << ", organizer: " << item.get_organizer().name();
+            std::cout << ", possible attendees: " << item.get_display_to()
+                      << std::endl;
+        }
     }
     catch (std::exception& exc)
     {
@@ -63,4 +72,3 @@ int main()
 }
 
 // vim:et ts=4 sw=4
-
