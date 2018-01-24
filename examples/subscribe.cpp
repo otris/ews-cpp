e4d54a69122f41e99320af57df2912b92b984a4b
Marcel Schlitzer <schlitzer@otris.de>
2018-01-24 10:43:05 +0100

    Move overloaded to anonymous namespace

--- examples/subscribe.cpp
+++ examples/subscribe.cpp
@@ -23,11 +23,14 @@
 #include <ostream>
 #include <string>
 
+namespace
+{
 template <class... Ts> struct overloaded : Ts...
 {
     using Ts::operator()...;
 };
 template <class... Ts> overloaded(Ts...)->overloaded<Ts...>;
+}
 
 int main()
 {
