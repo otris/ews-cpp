643e60b6a6fa4433e099df29fcb77a121a8875b5
Marcel Schlitzer <schlitzer@otris.de>
2019-05-13 13:37:54 +0200

    Add updated_extended_properties example

---
+++ examples/update_extended_property.cpp
@@ -0,0 +1,66 @@
+
+//   Copyright 2016 otris software AG
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#include <ews/ews.hpp>
+#include <ews/ews_test_support.hpp>
+#include <exception>
+#include <iostream>
+#include <ostream>
+#include <stdlib.h>
+#include <string>
+
+int main()
+{
+    int res = EXIT_SUCCESS;
+    ews::set_up();
+
+    try
+    {
+        const auto env = ews::test::environment();
+        auto service = ews::service(env.server_uri, env.domain, env.username,
+                                    env.password);
+
+        ews::distinguished_folder_id contacts_folder =
+            ews::standard_folder::contacts;
+        auto restriction =
+            ews::is_equal_to(ews::contact_property_path::email_address_1,
+                             "superhero@ducktales.com");
+        auto item_ids = service.find_item(contacts_folder, restriction);
+
+        std::cout << "Found " << item_ids.size() << " item(s)\n";
+
+        for (const auto& id : item_ids)
+        {
+            auto contact = service.get_contact(id);
+
+            ews::extended_field_uri::property_type title_prop_type("String");
+            ews::extended_field_uri::property_tag title_prop_id("14917");
+            ews::extended_field_uri title_prop_uri(title_prop_id, title_prop_type);
+            ews::extended_property title_prop(title_prop_uri, { "el presidente" });
+            ews::update update(title_prop);
+
+            auto new_id = service.update_item(id, update);
+            (void)new_id;
+        }
+    }
+    catch (std::exception& exc)
+    {
+        std::cout << exc.what() << std::endl;
+        res = EXIT_FAILURE;
+    }
+
+    ews::tear_down();
+    return res;
+}
